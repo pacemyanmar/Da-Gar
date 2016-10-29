d3e2ad41be59181d03fe3b48ca2f084a8562210d
Sithu Thwin <sithu@thwin.net>
2016-10-29 16:23:22 +0630

    add bower support for assets management

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,24 @@ require('laravel-elixir-vue');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.styles([
+    	
+    	],'public/css/vendor.css','resources/assets/vendor/')
+    	.scripts([
+            // Order is important
+            //'underscore/underscore-min.js',
+            'jquery/dist/jquery.min.js',
+            'jquery-ui/ui/minified/jquery-ui.min.js',
+            'jquery-ui/ui/minified/jquery.ui.sortable.min.js',
+            //'backbone/backbone.js',
+            //'backbone-deep-model/distribution/deep-model.js',            
+            'select2/dist/js/select2.min.js',
+            'iCheck/icheck.min.js',
+            'bootstrap/dist/js/bootstrap.min.js',
+            'formBuilder/dist/form-builder.min.js',
+            'formBuilder/dist/form-render.min.js',
+            'adminlte/dist/js/app.min.js'
+    		],'public/js/vendor.js','resources/assets/vendor/')
+    	.sass('app.scss')
+       	.webpack('app.js');
 });
