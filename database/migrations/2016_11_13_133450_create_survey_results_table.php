5883c18939416a21a6c1672de02f34f9d361babe
Sithu Thwin <sithu@thwin.net>
2016-11-22 22:57:51 +0630

    add samplable_data column and comments in survey result migration

--- database/migrations/2016_11_13_133450_create_survey_results_table.php
+++ database/migrations/2016_11_13_133450_create_survey_results_table.php
@@ -14,12 +14,16 @@ class CreateSurveyResultsTable extends Migration
     public function up()
     {
         Schema::create('survey_results', function (Blueprint $table) {
+            /**
+             * Composite unique key qnum, samplable_id, samplable_type, survey_input_id, project_id
+             */
             $table->increments('id');
             $table->text('value');
             $table->string('qnum')->index();
-            $table->integer('sort')->index();
-            $table->integer('samplable_id')->unsigned();
-            $table->string('samplable_type');
+            $table->integer('sort')->index(); //get from question table sort column
+            $table->integer('samplable_id')->unsigned(); //unique id for each sample type in a project (eg. if type is voter, this is id from voter list table)
+            $table->string('samplable_type'); // get from project or manual input
+            $table->text('samplable_data');// store all related sample data
             $table->string('survey_input_id')->index();
             $table->integer('project_id')->unsigned();
             $table->foreign('survey_input_id')->references('id')->on('survey_inputs');
