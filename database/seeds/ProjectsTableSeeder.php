4d9fce7aac9a481145817d4b003bebb33cde440e
Sithu Thwin <sithu@thwin.net>
2016-12-23 07:19:54 +0630

    remove function to fill in render column

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\SurveyInput;
 use App\Traits\QuestionsTrait;
 use Illuminate\Database\Seeder;
 
@@ -16,53 +15,31 @@ class ProjectsTableSeeder extends Seeder
     {
         $project = factory(App\Models\Project::class)->create();
         $questions = factory(App\Models\Question::class, 'question', 30)
-            ->create(['project_id' => $project->id])
-            ->each(function ($question) use ($project) {
-                $question->update(['render' => $this->to_render([
-                    'qnum' => $question->qnum,
-                    'layout' => $question->layout,
-                    'section' => $question->section,
-                    'project' => $project,
-                    'raw_ans' => $question->raw_ans,
-                ])]);
-            });
+            ->create(['project_id' => $project->id]);
         /**
          * 'raw_ans' => $raw_ans,
         'qnum' => $qnum,
         'layout' => $layout,
         'section' => $section,
         'project_id' => $this->definitions
-
-
-
          */
 
         foreach ($questions as $question) {
-            $render = $question->render;
-            $inputs = [];
-            foreach ($render as $k => $input) {
-                //remove className from array
-                unset($input['className']);
-                if ($input['type'] == 'radio-group') {
-                    foreach ($input['values'] as $i => $value) {
-                        $value['name'] = $input['name'];
-                        $value['sort'] = $question->sort . $k;
-                        $inputs[] = new SurveyInput($value);
-                    }
-                } else {
-                    if (!isset($input['value'])) {
-                        $input['value'] = $k;
-                    }
-
-                    if (!isset($input['sort'])) {
-                        $input['sort'] = $question->sort . $k;
-                    }
-
-                    $inputs[] = new SurveyInput($input);
-                }
-            }
-
-            $question->surveyInputs()->saveMany($inputs);
+            $render = $this->to_render(
+                [
+                    'question' => $question,
+                    'section' => $question->section,
+                    'project' => $project,
+                ],
+                [
+                    'qnum' => $question->qnum,
+                    'layout' => $question->layout,
+                    'raw_ans' => $question->raw_ans,
+                ]
+            );
+            $inputs = $this->getInputs($render);
+            $q = $question->surveyInputs()->saveMany($inputs);
         }
+
     }
 }
