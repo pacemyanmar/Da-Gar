4876b5dbb37fa0adf09c85d0c3545c1dba04c5cb
Sithu Thwin <sithu@thwin.net>
2016-12-25 23:29:57 +0630

    reload after question edit

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -292,7 +292,7 @@
             window.beforeunload = function(){ return void 0;}
             resetForm($( "#qModalForm" ))
             setTimeout(function(){
-              //window.location.reload();
+              window.location.reload();
             }, 1800);
           }
         });
