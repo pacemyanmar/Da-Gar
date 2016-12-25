6270a5bfca262b0e6e4a59682fc9b64537905d42
Sithu Thwin <sithu@thwin.net>
2016-12-25 19:49:37 +0630

    add custom morphMany method to use in SurveyResult

---
+++ app/Traits/SampleMorphManyTrait.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+trait SampleMorphManyTrait
+{
+    /**
+     * Define a polymorphic one-to-many relationship.
+     *
+     * @param  string  $related
+     * @param  string  $name
+     * @param  string  $type
+     * @param  string  $id
+     * @param  string  $localKey
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function morphMany($instance, $name, $type = null, $id = null, $localKey = null)
+    {
+
+        // Here we will gather up the morph type and ID for the relationship so that we
+        // can properly query the intermediate table of a relation. Finally, we will
+        // get the table and create the relationship instances for the developers.
+        list($type, $id) = $this->getMorphs($name, $type, $id);
+
+        $table = $instance->getTable();
+
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new MorphMany($instance->newQuery(), $this, $table . '.' . $type, $table . '.' . $id, $localKey);
+    }
+}
