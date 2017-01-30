44a3c996d476aa3285211e55c960f575970cf961
Sithu Thwin <sithu@thwin.net>
2017-01-31 01:37:09 +0630

    remove other box support for checkbox

--- resources/views/questions/double_checkbox.blade.php
+++ resources/views/questions/double_checkbox.blade.php
@@ -3,25 +3,6 @@
 	<label class="normal-text" for="{!! $element->id !!}">{!! $element->label !!} @if($element->value != '') <span class="label label-primary badge">{!! $element->value !!}</span> @endif
 	@if($element->status != 'published') <span class="label label-warning badge">{!! $element->status !!}</span> @endif
 	<span class="hide label label-danger badge {!! $element->inputid .' '.$element->id!!}">{!! "Data not match!" !!}</span>
-		@if($element->other)
-		{!! Form::text("result[".$element->inputid."]", (isset($results))?$results->{$element->inputid}:null, ['class' => $element->className.' form-control input-sm', 'autocomplete' => 'off', 'id' => $element->id.'other', 'style' => 'width:80%']) !!}
-			@push('document-ready')
-				$("input[name='result[{!! $element->inputid !!}]']").change(function(e){
-					if($("input[name='result[{!! $element->inputid !!}]']:checked").val() == {!! $element->value !!}) {
-						$("#{!! $element->id.'other' !!}").prop('disabled', false).prop('required', true).addClass('has-error');
-					} else {
-						$("#{!! $element->id.'other' !!}").prop('disabled', true).prop('required', false).removeClass('has-error');
-					}
-				});
-
-				if($("#{!! $element->id.'other' !!}").val() != "") {
-					$("#{!! $element->id.'other' !!}").prop('required', true).addClass('has-error');
-					$("#{!! $element->id !!}").prop('checked', true);
-				} else {
-					$("#{!! $element->id.'other' !!}").prop('disabled', true).prop('required', false).removeClass('has-error');
-				}
-			@endpush
-		@endif
     </label>
 </div>
 @if(!empty($element->skip) && !isset($editing))
