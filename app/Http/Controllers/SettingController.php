4aa17225876ca96c6413f0aa1ac91fbaad2308a5
Sithu Thwin <sithu@thwin.net>
2017-03-26 23:18:03 +0630

    remove zawgyi unicode conversion from translation

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -9,7 +9,6 @@ use App\Http\Requests\UpdateSettingRequest;
 use App\Repositories\SettingRepository;
 use Flash;
 use Illuminate\Http\Request;
-use Kanaung\Facades\Converter;
 use Krucas\Settings\Facades\Settings;
 use Response;
 
@@ -191,7 +190,8 @@ class SettingController extends AppBaseController
                         $extras = $classInstance->extras;
                         foreach ($columns as $column => $translation) {
 
-                            $extras['lang'][$locale][$column] = Converter::convert($translation, 'zawgyi', 'unicode');
+                            //$extras['lang'][$locale][$column] = Converter::convert($translation, 'zawgyi', 'unicode');
+                            $extras['lang'][$locale][$column] = $translation;
                         }
 
                         $classInstance->extras = $extras;
@@ -204,7 +204,8 @@ class SettingController extends AppBaseController
                         foreach ($columns as $column => $translation) {
                             $translation_column = $column . '_trans';
                             $old_translation = $classInstance->{$translation_column};
-                            $new_translation[$locale] = Converter::convert($translation, 'zawgyi', 'unicode');
+                            //$new_translation[$locale] = Converter::convert($translation, 'zawgyi', 'unicode');
+                            $new_translation[$locale] = $translation;
                             if (!empty($old_translation)) {
                                 $updated_translation = array_merge($old_translation, $new_translation);
                             } else {
@@ -222,7 +223,8 @@ class SettingController extends AppBaseController
                 foreach ($columns as $column => $translation) {
                     $translation_column = $column . '_trans';
                     $old_translation = $classInstance->{$translation_column};
-                    $new_translation[$locale] = Converter::convert($translation, 'zawgyi', 'unicode');
+                    //$new_translation[$locale] = Converter::convert($translation, 'zawgyi', 'unicode');
+                    $new_translation[$locale] = $translation;
                     if (!empty($old_translation)) {
                         $updated_translation = array_merge($old_translation, $new_translation);
                     } else {
