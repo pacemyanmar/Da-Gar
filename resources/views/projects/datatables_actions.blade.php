86459a2232a9a759a9f0fddaca437e32074ae294
Sithu Thwin <sithu@thwin.net>
2017-06-01 13:26:08 +0630

    allow response rate for supervisor

--- resources/views/projects/datatables_actions.blade.php
+++ resources/views/projects/datatables_actions.blade.php
@@ -20,7 +20,7 @@
             </a>
         @endif
     </div>
-    @if(Auth::user()->role->level > 7)
+    @if(Auth::user()->role->level >= 7)
         <div class="btn-group">
             <a href="{{ route('projects.response.filter', [$id, 'level1']) }}" class='btn btn-default btn-sm'>
                 <i class="glyphicon glyphicon-equalizer"></i> {!! trans('messages.response') !!}
