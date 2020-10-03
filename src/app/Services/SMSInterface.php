bdde6189cd02a3af2d4728d5317a4f196e2f9368
Sithu Thwin <sithu@thwin.net>
2020-10-03 21:48:45 +0630

    fix interface method

--- src/app/Services/SMSInterface.php
+++ src/app/Services/SMSInterface.php
@@ -9,7 +9,7 @@ Interface SMSInterface {
 
     public function setApiUrl($api_url);
 
-    public function setClientKey($client_key);
+    public function setAccessToken();
     
     public function receive($request);
 
