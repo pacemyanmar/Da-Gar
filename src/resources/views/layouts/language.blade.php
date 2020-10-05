078b5fe6455aee1ee44279ca57d10e95e33b78a4
Sithu Thwin <sithu@thwin.net>
2020-10-05 19:13:39 +0630

    update language switcher

--- src/resources/views/layouts/language.blade.php
+++ src/resources/views/layouts/language.blade.php
@@ -1,4 +1,4 @@
   <ul class="dropdown-menu">
-    <li><a href="{{ url('lang/en') }}"><img src="{{ asset('images/flags/us.png') }}" alt=""> {{ trans('locale.en') }}</a></li>
+    <li><a href="{{ url('lang/'.config('sms.primary_locale.locale')) }}"><img src="{{ asset('images/flags/'.config('sms.primary_locale.locale').'.png') }}" alt=""> {{ trans('locale..config('sms.primary_locale.locale')) }}</a></li>
     <li><a href="{{ url('lang/'.config('sms.second_locale.locale')) }}"><img src="{{ asset('images/flags/'.config('sms.second_locale.locale').'.png') }}" alt=""> {{ trans('locale.'.config('sms.second_locale.locale')) }}</a></li>
   </ul>
