c71b1f3757f4ac5561e22e2093852cc32b6168a6
Sithu Thwin <sithu@thwin.net>
2020-10-10 23:12:46 +0630

    hide add new button

--- src/resources/views/sample_details/index.blade.php
+++ src/resources/views/sample_details/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Sample Details</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('sample-details.create', $project->id) !!}">Add New</a>
+           <!--a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('sample-details.create', $project->id) !!}">Add New</a-->
         </h1>
     </section>
     <div class="content">
