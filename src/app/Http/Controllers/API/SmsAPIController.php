b55373f522d091e58e9020041aa5fa0abd2a7106
Sithu Thwin <sithu@thwin.net>
2020-10-15 15:59:20 +0630

    update SMS reply messages

--- src/app/Http/Controllers/API/SmsAPIController.php
+++ src/app/Http/Controllers/API/SmsAPIController.php
@@ -376,7 +376,7 @@ class SmsAPIController extends AppBaseController
 
         if (empty($observer_phone) && config('sms.verify_phone')) {
             // if project is empty
-            $reply['message'] = 'Wrong Phone Number!';
+            $reply['message'] = $this->encoding('sms.phone_error', $encoding);
             $reply['status'] = 'error';
             return $reply;
         }
