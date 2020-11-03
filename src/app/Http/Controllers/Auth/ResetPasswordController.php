7e2ed6556b9a4bcd69443b9dda3a28895cdb0a26
Sithu Thwin <sithu@thwin.net>
2020-11-03 22:36:00 +0630

    add redirectTo

--- src/app/Http/Controllers/Auth/ResetPasswordController.php
+++ src/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -20,6 +20,8 @@ class ResetPasswordController extends Controller
 
     use ResetsPasswords;
 
+    protected $redirectTo = '/projects';
+
     /**
      * Create a new controller instance.
      *
